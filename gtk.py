398a00d2af4df5dfed353a6bba10ff319816d3f5
rbn42 <bl100@students.waikato.ac.nz>
2017-09-10 15:32:17 +1200

    reverse color

--- gtk.py
+++ gtk.py
@@ -68,19 +68,36 @@ class Squareset(Gtk.DrawingArea):
         w, h = alloc.width, alloc.height
         data = next(read_data)
         fft = np.absolute(np.fft.rfft(data, n=len(data)))
-        bins = np.convolve(fft, kernel)
+        bins = np.convolve(fft, kernel,'valid')
         cr.set_source_rgba(1, 1, 1, opacity)
 
-        cr.move_to(0, h / 2)  # middle left
-        width = 2 * w / len(bins)
-        for i, bin in enumerate(bins[:len(bins) // 2]):
-            height = h * bin / float(SAMPLE_MAX) / 10
-            cr.line_to(i * width, h / 2 - height / 2)
-        cr.line_to(w, h / 2)  # middle right
-        for i, bin in enumerate(bins[len(bins) // 2:]):
-            height = h * bin / float(SAMPLE_MAX) / 10
-            cr.line_to(w - i * width, h / 2 + height / 2)
-        cr.close_path()
+        reverse=True
+        if reverse:
+            cr.move_to(0, 0) 
+            width = 2 * w / len(bins)
+            for i, bin in enumerate(bins[:len(bins) // 2]):
+                height = h * bin / float(SAMPLE_MAX) / 10
+                cr.line_to(i * width, h / 2 - height / 2)
+            cr.line_to(w,  0)
+            cr.close_path()
+
+            cr.move_to(w, h )
+            for i, bin in enumerate(bins[len(bins) // 2:]):
+                height = h * bin / float(SAMPLE_MAX) / 10
+                cr.line_to(w - i * width, h / 2 + height / 2)
+            cr.line_to(0,  h)
+            cr.close_path()
+        else:
+            cr.move_to(0, h / 2)  # middle left
+            width = 2 * w / len(bins)
+            for i, bin in enumerate(bins[:len(bins) // 2]):
+                height = h * bin / float(SAMPLE_MAX) / 10
+                cr.line_to(i * width, h / 2 - height / 2)
+            cr.line_to(w, h / 2)  # middle right
+            for i, bin in enumerate(bins[len(bins) // 2:]):
+                height = h * bin / float(SAMPLE_MAX) / 10
+                cr.line_to(w - i * width, h / 2 + height / 2)
+            cr.close_path()
         cr.fill()
 
 
