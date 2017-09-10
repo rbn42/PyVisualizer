70765d59b3cff755838aafe8c0b1b9c7a4b8b0fa
rbn42 <bl100@students.waikato.ac.nz>
2017-09-10 12:26:13 +1200

    reuse image

--- mpd.py
+++ mpd.py
@@ -5,33 +5,13 @@ import numpy as np
 from PySide import QtCore, QtGui
 from PySide.QtCore import *
 from visualizer import *
+
+
 app = QtGui.QApplication(sys.argv)
-
-
-def record_mpd():
-    FIFO = '/tmp/mpd.fifo'
-    stereo = True
-    fps = 25
-    # format                  "44100:16:2"
-    m_samples = 44100 // fps
-    if stereo:
-        m_samples *= 2
-    f = open(FIFO, 'rb')
-
-    def read_data():
-        line = f.read(m_samples)
-        data = np.frombuffer(line, 'int16').astype(float)
-        return data
-    return read_data
-
-
-read_data = record_mpd()
-
-window = Spectrogram(read_data)
+window = Spectrogram(None)
 window.setAttribute(Qt.WA_TranslucentBackground)
 window.setWindowFlags(Qt.FramelessWindowHint)
 window.show()
-
 from Xlib import X, display, Xutil, protocol
 disp = display.Display()
 winid = window.winId()
@@ -39,14 +19,27 @@ window_xlib = disp.create_resource_object('window', winid)
 window_xlib.set_wm_class('qmlterm_background', 'qmlterm_background')
 disp.flush()
 disp.close()
+#import setproctitle
+#setproctitle.setproctitle("background-visualizer")
 
-import setproctitle
-setproctitle.setproctitle("background-visualizer")
+
+FIFO = '/tmp/mpd.fifo'
+stereo = True
+fps = 25
+m_samples = 44100 // fps
+if stereo:
+    m_samples *= 2
+f = open(FIFO, 'rb')
 
 import os.path
+import time
 while True:
+    time.sleep(1/fps)
     if os.path.exists('/dev/shm/quit-visualizer'):
         break
+    line = f.read(m_samples)
+    f.flush()
+    data = np.frombuffer(line, 'int16').astype(float)
+    if data is not None:
+        window.refresh(data)
     app.processEvents()
-import sys
-sys.exit()
--- visualizer.py
+++ visualizer.py
@@ -25,53 +25,21 @@ class Visualizer(QtGui.QLabel):
 
     def __init__(self, get_data, update_interval=33):
         super(Visualizer, self).__init__()
-
-        self.get_data = get_data
-        self.update_interval = update_interval  # 33ms ~= 30 fps
-        self.sizeHint = lambda: QtCore.QSize(400, 400)
         self.setStyleSheet('background-color: black;');
         self.setWindowTitle('PyVisualizer')
+        self.img = QtGui.QImage(1920, 1080,
+                                QtGui.QImage.Format_ARGB32)
 
-    def show(self):
-        """Show the label and begin updating the visualization."""
-        super(Visualizer, self).show()
-        self.refresh()
-
-    def refresh(self):
+    def refresh(self, data):
         """Generate a frame, display it, and set queue the next frame"""
-        data = self.get_data()
-        interval = self.update_interval
         if data is not None:
-            t1 = time.clock()
             self.setPixmap(QtGui.QPixmap.fromImage(self.generate(data)))
-            # decrease the time till next frame by the processing tmie so that the framerate stays consistent
-            interval -= 1000 * (time.clock() - t1)
-        if self.isVisible():
-            QtCore.QTimer.singleShot(self.update_interval, self.refresh)
-
-    def generate(self, data):
-        """This is the abstract function that child classes will override to
-        draw a frame of the visualization.
-
-        The function takes an array of data and returns a QImage to display"""
-        raise NotImplementedError()
 
 
 class Spectrogram(Visualizer):
-    def generate(self, data):
+    def getPath(self, data):
         fft = np.absolute(np.fft.rfft(data, n=len(data)))
-        bins = np.zeros(len(fft))
-        step = int(len(fft) / len(bins))
-        for i in range(len(bins)):
-            bins[i] = np.mean(fft[i:i + step])
-
-        img = QtGui.QImage(self.width(), self.height(),
-                           QtGui.QImage.Format_ARGB32)
-        img.fill(0)
-        painter = QtGui.QPainter(img)
-        painter.setPen(QtCore.Qt.NoPen)
-        painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 25)))
-
+        bins = fft
         polygonPath = QtGui.QPainterPath()
         polygonPath.moveTo(0, self.height() / 2)  # left middle
         width = 2 * self.width() / float(len(bins))
@@ -84,9 +52,15 @@ class Spectrogram(Visualizer):
             polygonPath.lineTo(self.width() - i * width,
                                self.height() / 2 + height / 2)
         polygonPath.closeSubpath()
+        return polygonPath
 
-        painter.drawPath(polygonPath)
-
+    def generate(self, data):
+        img = self.img
+        img.fill(0)
+        painter = QtGui.QPainter(img)
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 25)))
+        path = self.getPath(data)
+        painter.drawPath(path)
         del painter
-
         return img
